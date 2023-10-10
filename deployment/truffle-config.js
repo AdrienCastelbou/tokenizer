b14b40a9f2cf14333edd7b7f04a18c69b1216664
Adrien CASTELBOU <acastelb@student.42.fr>
2023-10-10 16:55:19 +0200

    use env var

--- deployment/truffle-config.js
+++ deployment/truffle-config.js
@@ -1,16 +1,16 @@
+require('dotenv').config()
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const fs = require('fs')
 
 module.exports = {
     plugins: [
         'truffle-plugin-verify'
     ],
     api_keys: {
-        bscscan: "XKC84MIGZI1ND62T6PH3GVSA7DM17QQABP"
+        bscscan: process.env.BSCSCANAPIKEY
     },
     networks: {
         testnet: {
-            provider: () => new HDWalletProvider("rate muffin girl unknown runway job ethics marble demand castle awkward angle", `https://data-seed-prebsc-1-s1.bnbchain.org:8545`),
+            provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://data-seed-prebsc-1-s1.bnbchain.org:8545`),
             network_id: 97,
             confirmations: 2,
             timeoutBlocks: 200,
