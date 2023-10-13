c92c0dcd8ee5252f478f55a417526174b79200e0
Adrien CASTELBOU <acastelb@student.42.fr>
2023-10-13 10:57:55 +0200

    update deployment

--- deployment/migrations/2_deploy_multisig.js
+++ deployment/migrations/2_deploy_multisig.js
@@ -1,7 +1,15 @@
+const argv = require('minimist')(process.argv.slice(2), {string: ['custom_argument']});
 const Multisig = artifacts.require("MultiSigWallet");
 
+let args = argv['custom_argument'].trim().split(/\s+/)
+
+if (args.length < 2) {
+  console.log("Wrong usage of custom_arguemnt (only used for Multisig contract deployment): truffle migrate [...] --custom_argument 'numConfirmations address1 address2 ...addressX'")
+  process.exit();
+}
+const numConirmations = args.shift()
+const addresses = args
+
 module.exports = function (deployer) {
-  const addresses = ["0x520f0d93B7c193aF531D9efE4ea7E2163A25349a", "0x9b877504594Fe6d616a1c13DfDE00B4D0620d0Bf", "0x374C086BE3af90849433E9BdE0a49f1207C08485"]
-  const numConirmations = 2;
   deployer.deploy(Multisig, addresses, numConirmations);
 };
