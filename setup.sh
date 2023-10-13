c145b631192aa473545d92623aa427a741400063
Adrien CASTELBOU <acastelb@student.42.fr>
2023-10-13 14:07:06 +0200

    ok

--- setup.sh
+++ setup.sh
@@ -1,16 +1,22 @@
 #!/bin/bash                                                                                                         
 
-if [[ "$#" -eq 0 ]]; then
-    echo "[ MANDATORY ] Copying smart contract code for compilation and deployement.."
-    cp ./code/coin42.sol ./deployment/contracts
-    echo "[ MANDATORY ] Done"
-elif [[ "$#" -eq 1 ]] && [[ "$1" = "--bonus" ]]; then
+if [ "$#" -gt 1 -o \( "$#" -eq 1 -a "$1" != "--bonus" \) ]; then
+    echo "Wrong usage : run setup.sh for mandatory part or setup.sh --bonus for the bonus part"
+    exit
+fi
+
+echo "[ MANDATORY ] Copying smart contract code for compilation and deployement.."
+cp ./code/coin42.sol ./deployment/contracts
+echo "[ MANDATORY ] Done"
+echo "[ MANDATORY ] Installing packages ..."
+npm install --prefix ./deployment ./deployment
+echo "[ MANDATORY ] Done"
+
+if [[ "$#" -eq 1 ]] && [[ "$1" = "--bonus" ]]; then
     echo "[ BONUS ] Copying multisig smart contract code for compilation and deployement.."
     cp ./code/multisig42.sol ./deployment/contracts
     echo "[ BONUS ] Done"
     echo "[ BONUS ] Installing packages..."
     npm install --prefix ./bonus ./bonus
     echo "[ BONUS ] Done"
-else
-    echo "Wrong usage : run setup.sh for mandatory part or setup.sh --bonus for the bonus part"
 fi
